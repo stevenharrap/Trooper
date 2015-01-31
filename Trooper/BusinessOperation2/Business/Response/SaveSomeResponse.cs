d34e539386cdde25f9391364046be3dcda7cf446
steven_harrap@hotmail.com <R2*9xcdyugfvgm>
2015-01-31 23:41:26 +1000

    Facade and business core unit tests

---
+++ Trooper/BusinessOperation2/Business/Response/SaveSomeResponse.cs
@@ -0,0 +1,30 @@
+﻿//--------------------------------------------------------------------------------------
+// <copyright file="SaveResponse.cs" company="Trooper Inc">
+//     Copyright (c) Trooper 2014 - Onwards
+// </copyright>
+//--------------------------------------------------------------------------------------
+
+namespace Trooper.BusinessOperation2.Business.Response
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using Trooper.BusinessOperation2.Interface.Business.Response;
+    using Trooper.BusinessOperation2.Interface.OperationResponse;
+    
+    /// <summary>
+    /// The many response is an implementation of the IOperationResponse interface
+    /// for responding to requests for 1 entity of a particular type.
+    /// </summary>
+    /// <typeparam name="TEntity">
+    /// The type of entity to return
+    /// </typeparam>
+    [Serializable]
+    [DataContract]
+    [ServiceContract(Namespace = Constants.ServiceContractNameSpace)]
+    public class SaveSomeResponse<T> : Response, ISaveSomeResponse<T>
+    {
+        public IEnumerable<ISaveSomeItem<T>> Items { get; set; }
+    }
+}
