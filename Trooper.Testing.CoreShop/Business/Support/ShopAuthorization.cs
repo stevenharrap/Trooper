b2245070d329577d5501cb235343b2b0cd29432f
stevenharrap <steven_harrap@hotmail.com>
2015-05-26 17:35:51 +1000

    Authorization work

--- Trooper.Testing.CoreShop/Business/Support/ShopAuthorization.cs
+++ Trooper.Testing.CoreShop/Business/Support/ShopAuthorization.cs
@@ -15,29 +15,30 @@ namespace Trooper.Testing.CustomShopApi.Business.Support
 
     public class ShopAuthorization : Authorization<Shop>, IShopAuthorization
     {
-		//private readonly IList<IUserRole> roles = new IUserRole[]
-		//{
-		//	new UserRole {Action = Action.AllActions, Allow = true, Users = new[] {TestBase.ValidUsername}},
-		//	new UserRole {Action = Action.AllActions, Allow = false, Users = new[] {TestBase.InvalidUsername}},
-		//	new UserRole {Action = Action.GetByKeyAction, Allow = true, Users = new[] {"ReaderUser"}}
-		//};
-
-		//public override IList<IUserRole> Roles
-		//{
-		//	get { return roles; }
-		//	set { throw new NotImplementedException(); }
-		//}
-
-		public override bool IsAllowed(IRequestArg<Shop> arg, ICredential credential, IResponse response)
+		private readonly IList<IUserRole> roles = new IUserRole[]
 		{
-			if (credential.Username == TestBase.InvalidUsername)
-			{
-				MessageUtility.Errors.Add(string.Format("'{0}' is not allowed", TestBase.InvalidUsername), UserDeniedCode, response);
+			new UserRole {Action = Action.AllActions, Allow = true, Users = new[] {TestBase.ValidUsername}},
+			new UserRole {Action = Action.AllActions, Allow = false, Users = new[] {TestBase.InvalidUsername}},
+			new UserRole {Action = Action.GetByKeyAction, Allow = true, Users = new[] {"ReaderUser"}}
+		};
 
-				return false;
-			}
 
-			return base.IsAllowed(arg, credential, response);
+		public override IList<IUserRole> Roles
+		{
+			get { return roles; }
+			set { throw new NotImplementedException(); }
 		}
+
+		//public override bool IsAllowed(IRequestArg<Shop> arg, ICredential credential, IResponse response)
+		//{
+		//	if (credential.Username == TestBase.InvalidUsername)
+		//	{
+		//		MessageUtility.Errors.Add(string.Format("'{0}' is not allowed", TestBase.InvalidUsername), UserDeniedCode, response);
+
+		//		return false;
+		//	}
+
+		//	return base.IsAllowed(arg, credential, response);
+		//}
     }
 }
---
+++ Trooper/Thorny/Business/Security/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trooper.Thorny.Business.Security
+{
+	public class Role
+	{
+		public List<Behaviour> Behaviours { get; set; }
+	}
+}
