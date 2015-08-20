fd010598e33cda11943fe6d24df4be6572e057f7
steven_harrap@hotmail.com <R2*9xcdyugfvgm>
2015-08-20 22:42:56 +1000

    More test interfaces

---
+++ Trooper/Interface/Thorny/TestSuit/BusinessCoreTestSuit/ISaving.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trooper.Interface.Thorny.TestSuit.BusinessCoreTestSuit
+{
+    public interface ISaving
+    {
+        #region Item is valid and does exist
+
+        /// <summary>
+        ///     Response.Item = added item
+        ///     Response.Ok = true
+        ///     Response.Messages = empty
+        /// </summary>
+        void DoesUpdateWhenItemIsValidAndExistsAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Access Denied]
+        /// </summary>
+        void DoesNotUpdateWhenItemIsValidAndExistsAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenItemIsValidAndExistsAndIdentityIsNull();
+
+        #endregion
+
+        #region Item is valid and does not exist
+
+        /// <summary>
+        ///     Response.Item = added item
+        ///     Response.Ok = true
+        ///     Response.Messages = empty
+        /// </summary>
+        void DoesAddWhenItemIsValidAndDoesNotExistAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Access Denied]
+        /// </summary>
+        void DoesNotAddWhenItemIsValidAndDoesNotExistAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotAddWhenItemIsValidAndDoesNotExistAndIdentityIsNull();
+
+        #endregion
+
+        #region Item is invalid and exists
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Invalid item]
+        /// </summary>
+        void DoesNotUpdateWhenItemIsInvalidAndExistsAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Access Denied]
+        /// </summary>
+        void DoesNotUpdateWhenItemIsInvalidAndExistsAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotUpdateAddWhenItemIsInvalidAndExistsAndIdentityIsNull();
+
+        #endregion
+
+        #region Item is invalid and does not exist
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Invalid item]
+        /// </summary>
+        void DoesNotAddWhenItemIsInvalidAndDoesNotExistAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Access Denied]
+        /// </summary>
+        void DoesNotAddWhenItemIsInvalidAndDoesNotExistAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotAddWhenItemIsInvalidAndDoesNotExistAndIdentityIsNull();
+
+        #endregion
+
+        #region item is null
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Item not supplied]
+        /// </summary>
+        void DoesNotSaveWhenItemIsNullAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Item not supplied]
+        /// </summary>
+        void DoesNotSaveWhenItemIsNullAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Item = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Item not supplied, Itentity not supplied]
+        /// </summary>
+        void DoesNotSaveWhenItemIsNullAndIdentityIsNull();
+
+        #endregion
+    }
+}
---
+++ Trooper/Interface/Thorny/TestSuit/BusinessCoreTestSuit/IUpdatingSome.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trooper.Interface.Thorny.TestSuit.BusinessCoreTestSuit
+{
+    public interface IUpdatingSome
+    {
+        #region Items are all valid and exist
+
+        /// <summary>
+        ///     Response.Items = updated items
+        ///     Response.Ok = true
+        ///     Response.Messages = empty
+        /// </summary>
+        void DoesUpdateWhenAllItemsAreValidAndExistAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Access denied]
+        /// </summary>
+        void DoesNotUpdateWhenAllItemsAreValidAndExistAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenAllItemsAreValidAndExistAndIdentityIsNull();
+
+        #endregion
+
+        #region Items are all valid and some dont exist
+
+        /// <summary>
+        ///     Response.Items = updated items
+        ///     Response.Ok = true
+        ///     Response.Messages = empty
+        /// </summary>
+        void DoesUpdateWhenAllItemsAreValidAndSomeDoNotExistAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Access denied]
+        /// </summary>
+        void DoesNotUpdateWhenAllItemsAreValidAndSomeDoNotExistAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenAllItemsAreValidAndSomeDoNotExistAndIdentityIsNull();
+
+        #endregion
+
+        #region Any items are invalid and all exist
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Some items invalid]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreInvalidAndAllExistAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Access denied]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreInvalidAllExistAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreInvalidAndAllExistAndIdentityIsNull();
+
+        #endregion
+
+        #region Any items are invalid and some don't exist
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Some items invalid]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreInvalidAndSomeDoNotExistAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Access denied]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreInvalidSomeDoNotExistAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Identity not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreInvalidAndSomeDoNotExistAndIdentityIsNull();
+
+        #endregion
+
+        #region Any items are null
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Some items null]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreNullAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Items = empty
+        ///     Response.Ok = true
+        ///     Response.Messages = [Items not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreNullAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Items = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Item not supplied, Itentity not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenAnyItemsAreNullAndIdentityIsNull();
+
+        #endregion
+
+        #region items is null
+
+        /// <summary>
+        ///     Response.Items = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Items not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenItemsIsNullAndIdentityIsAllowed();
+
+        /// <summary>
+        ///     Response.Items = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Items not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenItemsIsNullAndIdentityIsNotAllowed();
+
+        /// <summary>
+        ///     Response.Items = null
+        ///     Response.Ok = false
+        ///     Response.Messages = [Items not supplied]
+        /// </summary>
+        void DoesNotUpdateWhenItemsIsNullAndIdentityIsNull();
+
+        #endregion        
+    }
+}
